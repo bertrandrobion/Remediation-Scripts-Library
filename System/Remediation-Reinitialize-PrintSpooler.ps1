1478cfb7427022598ab245c06bfe53e50449a428
bertrandrobion <64419855+bertrandrobion@users.noreply.github.com>
2020-11-10 17:23:44 +0100

    Update Remediation-Reinitialize-PrintSpooler.ps1

--- System/Remediation-Reinitialize-PrintSpooler.ps1
+++ System/Remediation-Reinitialize-PrintSpooler.ps1
@@ -1,6 +1,6 @@
 <#
 .Synopsis
-   Aternity - Remediation Script: Reinitialize-PrintSpooler
+	Aternity - Remediation Script: Reinitialize-PrintSpooler
 .DESCRIPTION
 	Stop the print spooler service, delete the printing job in the queue, and restart spooler.
 	Use case: Fix printing job issue
@@ -11,36 +11,32 @@
 	* https://help.aternity.com/search?facetreset=yes&q=remediation
 
 .EXAMPLE
-   Deploy in Aternity (Configuration > Remediation > Add Action) 
-   Action Name: Reinitialize-PrintSpooler
-   Description:  Reinitialize the print spooler service
-   Run the script in the System account: checked
+	Deploy in Aternity (Configuration > Remediation > Add Action) 
+	Action Name: Reinitialize-PrintSpooler
+	Description:  Reinitialize the print spooler service
+	Run the script in the System account: checked
+.VERSION
+	Date : 11/10/2020 Version : 1.0
 #>
 
 try
 {
 	# Load Agent Module
-    Add-Type -Path $env:STEELCENTRAL_ATERNITY_AGENT_HOME\ActionExtensionsMethods.dll
+	Add-Type -Path $env:STEELCENTRAL_ATERNITY_AGENT_HOME\ActionExtensionsMethods.dll
 	
-	#region Remediation action logic
+	# region Remediation action logic
 
-		# Add your remediation code here and set the variable $result with the Output Message to be visible visible in Aternity's dashboards.
-		#
-		# For example:
-		# 	Clear-DnsClientCache
-		# 	$result="DNS Cache Cleared"
-
-Stop-Service -Name Spooler -Force  -ErrorAction Stop
-Remove-Item -Path "$env:SystemRoot\System32\spool\PRINTERS\*.*" -Force
-Start-Service -Name Spooler  -ErrorAction Stop
-$result = Get-Service Spooler | Out-String
-
-	#endregion
+	Stop-Service -Name Spooler -Force  -ErrorAction Stop
+	Remove-Item -Path "$env:SystemRoot\System32\spool\PRINTERS\*.*" -Force
+	Start-Service -Name Spooler  -ErrorAction Stop
+	$service = Get-Service Spooler
+	$result = $service.name + " is " + $service.status + "."
+	# endregion
 
 	# Set Output message
-    [ActionExtensionsMethods.ActionExtensionsMethods]::SetScriptOutput($result)
+	[ActionExtensionsMethods.ActionExtensionsMethods]::SetScriptOutput($result)
 }
 catch
 {
-    [ActionExtensionsMethods.ActionExtensionsMethods]::SetFailed($_.Exception.Message)
+	[ActionExtensionsMethods.ActionExtensionsMethods]::SetFailed($_.Exception.Message)
 }
